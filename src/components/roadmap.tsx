3565bf2f37276357912e3344686abeda843f4dfd
Rakshit <404rakshit@gmail.com>
2024-05-09 02:09:26 +0530

    fix: slider

--- src/components/roadmap.tsx
+++ src/components/roadmap.tsx
@@ -17,7 +17,51 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+import Autoplay from "embla-carousel-autoplay";
+
 export default function Roadmap() {
+  const slides = [
+    {
+      title: "Education and Research",
+      desc: "Knowledge is power. Start by educating yourself about cryptocurrencies, blockchain technology, and the factors influencing market movements. Dive into reputable resources, attend webinars, and stay updated with the latest news and trends.",
+    },
+    {
+      title: "Define Your Investment Goals",
+      desc: "What do you aim to achieve with your crypto investments? Whether it's long-term wealth accumulation, portfolio diversification, or short-term gains, clearly define your investment objectives to tailor your strategy accordingly.",
+    },
+    {
+      title: "Assess Your Risk Tolerance",
+      desc: "Cryptocurrency markets are known for their volatility. Assess your risk tolerance honestly and determine how much you're willing to invest in this high-risk, high-reward asset class.",
+    },
+    {
+      title: "Choose a Reliable Exchange",
+      desc: "Select a reputable cryptocurrency exchange that prioritizes security, offers a user-friendly interface, and provides a wide range of trading options. Conduct thorough research and read user reviews before making your decision.",
+    },
+    {
+      title: "Secure Your Wallet",
+      desc: "Safeguard your digital assets by setting up a secure cryptocurrency wallet. Choose between hardware wallets, software wallets, or mobile wallets, depending on your preferences and security needs.",
+    },
+    {
+      title: "Diversify Your Portfolio",
+      desc: "Spread your investments across different cryptocurrencies to mitigate risk and maximize potential returns. Consider allocating a portion of your portfolio to established coins like Bitcoin and Ethereum, as well as promising altcoins with growth potential.",
+    },
+    {
+      title: "Develop an Investment Strategy",
+      desc: "Craft a well-defined investment strategy based on your research, goals, and risk tolerance. Decide whether you'll be a long-term holder or an active trader, and establish clear entry and exit points.",
+    },
+    {
+      title: "Monitor Market Trends",
+      desc: "Stay informed about market trends, news, and developments in the cryptocurrency space. Regularly monitor the performance of your investments and be prepared to adjust your strategy in response to changing market conditions.",
+    },
+    {
+      title: "Stay Disciplined",
+      desc: "Emotions can cloud judgment and lead to impulsive decisions. Stay disciplined and stick to your investment strategy, even during periods of market volatility. Avoid chasing quick profits and focus on long-term growth.",
+    },
+    {
+      title: "Review and Adapt",
+      desc: "Regularly review your investment portfolio and make necessary adjustments to keep it aligned with your goals and risk tolerance. Be open to learning and adapting your strategy based on your experiences and market insights.",
+    },
+  ];
   return (
     <div className="bg-background p-4 md:p-10">
       <div className="mx-auto flex flex-col w-full max-w-6xl gap-10">
@@ -34,6 +78,14 @@ export default function Roadmap() {
         <div className="mx-auto w-full max-w-xl sm:max-w-5xl gap-6">
           {/* Slider */}
           <Carousel
+            plugins={[
+              Autoplay({
+                delay: 3000,
+                stopOnFocusIn: false,
+                stopOnMouseEnter: true,
+                stopOnInteraction: false,
+              }),
+            ]}
             opts={{
               align: "center",
               loop: true,
@@ -41,28 +93,24 @@ export default function Roadmap() {
             className="w-full mx-auto"
           >
             <CarouselContent>
-              {Array.from({ length: 5 }).map((_, index) => (
+              {slides.map(({ title, desc }, index) => (
                 <CarouselItem
                   key={index}
-                  className="max-sm:basis-4/5 basis-1/3"
+                  className="max-sm:basis-4/5 max-md:basis-1/2 basis-1/3 h-full"
                 >
-                  <div className="p-1">
-                    <Card className="bg-muted/40">
+                  <div className="p-1 h-full">
+                    <Card className="bg-muted/40 min-h-[270px]">
                       <CardHeader className="flex gap-2 items-center">
                         <div className="p-1.5 bg-gradient-to-tr from-yellow-600 to-orange-600 backdrop-blur-sm rounded-full flex items-center gap-2 shrink-0 max-w-72 mx-auto">
                           <div className="p-5 py-2 rounded-full text-3xl bg-muted flex-auto shrink-0 grow">
                             {index + 1}
                           </div>
                           <CardTitle className="line-clamp-2 text-xl leading-5">
-                            {"Lorem ipsum dolor sit. dasdasas dsa das d"}
+                            {title}
                           </CardTitle>
                         </div>
                         <CardDescription className="text-center">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Corrupti, odit quos, laborum deserunt officiis
-                          magnam harum earum alias aliquam ea qui et nobis
-                          repellat mollitia pariatur maxime expedita quibusdam
-                          eum.
+                          {desc}
                         </CardDescription>
                       </CardHeader>
                       {/* <CardContent className="flex items-center justify-center p-6">
@@ -75,6 +123,8 @@ export default function Roadmap() {
                 </CarouselItem>
               ))}
             </CarouselContent>
+            <CarouselPrevious className="max-sm:hidden" />
+            <CarouselNext className="max-sm:hidden" />
           </Carousel>
         </div>
       </div>
